abd2b55eb0219fb35281509491fd3659acb5ff3c
Northman94 <Zhandarm2000@gmail.com>
2023-04-27 01:04:56 +0300

    Randomiser added

--- Lesson2/main_L2.py
+++ Lesson2/main_L2.py
@@ -1,35 +1,33 @@
 
 from flask import Flask, render_template
+from random import randint
+
 app = Flask(__name__)
 
 numbList = []
 b = 0
 
-
 @app.route("/")
 @app.route("/status", methods = ["GET"])
 def show_state():
     print(f"List status:{numbList}")
-    #return f"List status: {numbList}"
     return render_template("status.html", numbList=numbList)
 
 
 @app.route("/add", methods = ["GET", "POST"])
 def addition():
-    global b
-    b += 1
+    b = randint(0,100)
     numbList.append(b)
     print(f"List status:{numbList}")
-    #return f"List status: {numbList}"
     return render_template("addition.html", addList=numbList)
 
 
 @app.route("/del", methods = ["GET","DELETE"])
 def delete():
     print(len(numbList))
+
     if len(numbList) >= 1:
         numbList.pop()
-    #return f"List status: {numbList}"
         return render_template("delete.html", delList=numbList)
     else:
         return render_template("status.html", numbList=numbList)
