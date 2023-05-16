de6d30f4c3f50e9670e1bd5da13a8a338816c598
Northman94 <Zhandarm2000@gmail.com>
2023-05-17 02:15:02 +0300

    Cleared comments

--- Lesson5/school/diary/views.py
+++ Lesson5/school/diary/views.py
@@ -12,46 +12,39 @@ def login(request):
     global user
 
     if request.method == 'POST':
-        # Info local save from HTML-Form:
+        # Info from login HTML-Form:
         l_name = request.POST.get('username')
         l_password = request.POST.get('password')
 
         # Log-In Button Pressed:
         if request.POST.get('action') == 'Login':
-            print("LOGIN PRESSED")
-
+            # User Present:
             if check_user_in_db(l_name, l_password):
-                # User Present:
-                print("User Present")
                 return redirect(show_profile)
             else:
                 # User Absent:
-                print("User Absent")
                 return render(request, 'login.html', {'message': 'No such User.'})
 
 
         # Register Button Pressed:
         if request.POST.get('action') == 'Register':
-            print("REGISTER PRESSED")
             # Check if user PRESENT in DB:
             if check_user_in_db(l_name, l_password):
                 return render(request, 'login.html', {'message': 'Username already exists.'})
             else:
-                # Create USER partially to have filled suggestions in next form
-                print("CREATE NEW LOCAL PRE USER")
+                # Create USER partially to have filled suggestions in the next form
                 user = User(name=l_name, password=l_password)
-
                 return redirect(alter_user)
 
-    print("RENDER LOGIN")
+    # RENDER LOGIN
     return render(request, 'login.html')
 
 
 def alter_user(request):
     global user
-    print("SET/ALTER PROFILE")
 
     if request.method == "POST":
+        # Info from alter HTML-Form:
         a_name = request.POST.get('username')
         a_password = request.POST.get('password')
         a_language = request.POST.get('language')
@@ -59,7 +52,7 @@ def alter_user(request):
 
         if request.POST.get('action') == 'Next':
             if check_user_in_db(a_name, a_password):
-                # User present in DB - update fields
+                # User present in DB => update fields
                 # Avoiding New Instance Creation
                 db_user = User.objects.get(id=user.id)
                 db_user.name = a_name
@@ -79,43 +72,40 @@ def alter_user(request):
                 user.password = a_password
                 user.language = a_language
                 user.grade = a_grade
+
                 user.save()
 
             return redirect(show_profile)
 
+    # RENDER ALTER
     return render(request, 'alter.html', {'user': user})
 
 
 
 def show_profile(request):
     global user
-    print("SHOW PROFILE")
 
     if request.method == 'POST':
-        print("SHOW PR POST")
         if request.POST.get('action') == 'Change User':
-            print("CHANGE USER")
             return redirect(alter_user)
 
         if request.POST.get('action') == 'Delete User':
-            print("DELETE USER")
             user = User.objects.filter(name=user.name, password=user.password).first()
             user.delete()
             return redirect(delete_profile)
 
-    print("RENDER PR")
+    print("RENDER PROFILE")
     return render(request, 'profile.html', {'user': user})
 
 
 def check_user_in_db(c_name, c_password):
     global user
-    print("CHECK USER!!!!!")
 
-    # SHOULD FILL HERE ALL FIELDS FROM DB:
+    # FILTERED FIELDS FROM DB:
     db_content = User.objects.filter(name=c_name, password=c_password).first()
-    print(f"DB CONTAINER: {db_content}")
+
     if db_content:
-        # Assign the user's fields to variables:
+        # Assign DB user's fields to variables:
         user.name = db_content.name
         user.password = db_content.password
         user.language = db_content.language
@@ -127,10 +117,9 @@ def check_user_in_db(c_name, c_password):
 def delete_profile(request):
     if request.method == 'POST':
         if request.POST.get('action') == 'Return to Login':
-            print("RETURN TO LOGIN FROM DEL")
             return redirect(login)
 
-    print("RENDER DELETE PR")
+    print("RENDER DELETE")
     return render(request, 'delete.html')
 
 
