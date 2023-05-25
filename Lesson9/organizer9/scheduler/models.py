d07235afc008706881b68716e1bc2fe663c4bf3b
Northman94 <Zhandarm2000@gmail.com>
2023-05-25 13:35:08 +0300

    L9_HW6  Proj transfer

---
+++ Lesson9/organizer9/scheduler/models.py
@@ -0,0 +1,20 @@
+# scheduler/models.py
+from django.db import models
+
+
+class MyUser(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    grade = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    # ForeignKey is a link to certain User in another table
+    # Cascade will delete all Obj with no relations
+    user_note = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    msg = models.CharField(max_length=250)
