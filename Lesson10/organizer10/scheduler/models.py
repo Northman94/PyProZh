9519cae83c0ef374cb1dd3b1decfffce783500cc
Northman94 <Zhandarm2000@gmail.com>
2023-05-31 18:53:05 +0300

    Moving L9 to L10
    + pip install -U pytest
    + update requirements

---
+++ Lesson10/organizer10/scheduler/models.py
@@ -0,0 +1,24 @@
+# scheduler/models.py
+from django.db import models
+
+
+class MyUser(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    grade = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    # ForeignKey is a link to certain User in another table
+    # (.CASCADE) will Delete all Obj-s with no relations
+    user_note = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=20)
+    msg = models.CharField(max_length=100)
+
+    assignee = models.CharField(max_length=100, blank=True)
+    e_mail = models.EmailField(max_length=254, blank=True)
