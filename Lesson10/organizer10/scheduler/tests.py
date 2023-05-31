3c5caa976e1f27a6e8e3baab493f18cc08067de4
Northman94 <Zhandarm2000@gmail.com>
2023-05-31 23:38:43 +0300

    status_code == 200 routing test

--- Lesson10/organizer10/scheduler/tests.py
+++ Lesson10/organizer10/scheduler/tests.py
@@ -1,3 +1,29 @@
+# tests.py
 from django.test import TestCase
+from django.urls import reverse
+import pytest
+from pytest_django.asserts import assertQuerysetEqual, assertTemplateUsed
+from .models import Note
+
+
+@pytest.mark.urls("scheduler.urls")
+def test_login_route(client):
+    response = client.get(reverse("login"))
+    assert response.status_code == 200
+    assertTemplateUsed(response, "login.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
